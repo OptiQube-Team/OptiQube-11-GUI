52c913c053300fb0f1b1f10979f8014722cc27e2
Dog Waffle <152004102+dogwaffleworld@users.noreply.github.com>
2025-05-28 15:48:30 +0200

    Update main window

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+// const { app, BrowserWindow } = require('electron');
+// const { ipcMain } = require('electron');
 
+// const path = require('path');
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 1150,
+//     height: 700,
+//     frame: false,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'Core/preload.js'),
+//     }
+//   });
+
+//   win.loadFile('Application/index.html');
+// }
+
+// ipcMain.on('close-window', () => {
+//   BrowserWindow.getFocusedWindow().close();
+// });
+// ipcMain.on('minimize-window', () => {
+//   BrowserWindow.getFocusedWindow().minimize();
+// });
+// ipcMain.on('maximize-window', () => {
+//   const window = BrowserWindow.getFocusedWindow();
+//   window.isMaximized() ? window.unmaximize() : window.maximize();
+// });
+
+
+// app.whenReady().then(() => {
+//   createWindow();
+// });
 
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
@@ -8,6 +40,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 1150,
         height: 700,
+        frame: false,
         webPreferences: {
             preload: path.join(__dirname, 'Core/preload.js'),
             contextIsolation: true,
